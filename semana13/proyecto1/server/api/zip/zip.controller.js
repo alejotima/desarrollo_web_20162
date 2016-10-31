e9cf2f34207f4adbdba9bbcd0c1cc9097d831efc
davidMorales <davidx1109@hotmail.com>
2016-10-30 20:02:34 -0500

    readme.md

--- semana13/proyecto1/server/api/zip/zip.controller.js
+++ semana13/proyecto1/server/api/zip/zip.controller.js
@@ -1,21 +1,18 @@
 /**
-*   GET     /zips       -> index
-*   POST    /zips       -> create
-*   PUT     /zips/:id   -> update
-*   DELETE  /zips/:id   -< destroy
-*/
-
+ *   GET     /zips       -> index
+ *   POST    /zips       -> create
+ *   PUT     /zips/:id   -> update
+ *   DELETE  /zips/:id   -< destroy
+ */
 'use strict';
-
 var Zip = require('./zip.model');
-
-exports.index = function(req, res){
+exports.index = function (req, res) {
     console.log('entro al index');
-    Zip.find(function(err,zips){
+    Zip.find(function (err, zips) {
         console.log(zips);
-        if(err){
+        if (err) {
             return handleError(res, err);
         }
         return res.status(200).json(zips);
-    }).limit(20);
+    }).limit(5);
 };
